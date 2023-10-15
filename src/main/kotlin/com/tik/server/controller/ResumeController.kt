4745322d78d81e367c93ed91b07e297e7e8ecf29
Sejeong Min <sj.min621@gmail.com>
2023-10-15 20:10:18 +0900

    feat: swagger ui 연결

--- src/main/kotlin/com/tik/server/controller/ResumeController.kt
+++ src/main/kotlin/com/tik/server/controller/ResumeController.kt
@@ -3,11 +3,13 @@ package com.tik.server.controller
 import com.tik.server.dto.ResumeCreateRequest
 import com.tik.server.dto.ResumeDetail
 import com.tik.server.service.ResumeService
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/resume")
+@Tag(name = "Resume", description = "이력서 관련 API입니다.")
 class ResumeController(
     private val resumeService: ResumeService
 ) {
@@ -22,4 +24,11 @@ class ResumeController(
     fun findAllResume(@RequestParam(name = "userId") userId: Int): List<ResumeDetail> {
         return resumeService.findAllResume(userId)
     }
+
+    @DeleteMapping("/delete/{resumeId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun deleteResume(@PathVariable resumeId: Long) {
+        resumeService.deleteResume(resumeId)
+    }
+
 }
