e9cd0b1e694d136ca2cfb0f3d98e33d9a4a819a2
Sejeong Min <sj.min621@gmail.com>
2023-11-12 01:04:42 +0900

    feat: swagger authorize 설정 추가

--- src/main/kotlin/com/tik/server/config/SwaggerConfig.kt
+++ src/main/kotlin/com/tik/server/config/SwaggerConfig.kt
@@ -1,17 +1,32 @@
 package com.tik.server.config
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
+import io.swagger.v3.oas.annotations.security.SecurityScheme
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
+private const val AUTH_TOKEN_HEADER = "X-AUTH-TOKEN"
+
+@SecurityScheme(
+    type = SecuritySchemeType.APIKEY, `in` = SecuritySchemeIn.HEADER,
+    name = AUTH_TOKEN_HEADER, description = "Auth Token",
+)
 @Configuration
 class SwaggerConfig {
     @Bean
     fun openAPI(): OpenAPI = OpenAPI()
         .components(Components())
         .info(apiInfo())
+        .security(
+            listOf(
+                SecurityRequirement().addList(AUTH_TOKEN_HEADER)
+            )
+        )
 
     private fun apiInfo() = Info()
         .title("면접왕 API 문서")
