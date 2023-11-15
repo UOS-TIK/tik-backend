224178e859f868dea6ac4d29f586d40ab88f2ffa
bhnimn <zxsaz1234@gmail.com>
2023-11-15 14:22:37 +0900

    cors 에러 수정

---
+++ src/main/kotlin/com/tik/server/security/CorsConfig.kt
@@ -0,0 +1,19 @@
+package com.tik.server.security
+
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig : WebMvcConfigurer {
+    override
+    fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("http://localhost::3000")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600)
+    }
+}
